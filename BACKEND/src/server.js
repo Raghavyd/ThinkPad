da847ca2b75e3d2cfc7f12bc525d517205d352b1
Raghavyd <kumaraman96450@gmail.com>
2025-09-17 01:58:38 +0530

    dd

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,39 +1,43 @@
 import express from "express";
 import notesRoute from "./routes/notesRoute.js";
-import cors from "cors"
-import { connect } from "mongoose";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import path from "path"
+import path from "path";
+
 dotenv.config();
-// console.log("MONGO_URI:", process.env.MONGO_URI);
+
 const app = express();
-const PORT=process.env.PORT ||5001
-const __dirname=path.resolve()
-//middleware
-if(process.env.NODE_ENV !=="production"){
-app.use(cors({
-    origin:"http://localhost:5173"
-}))
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
 
-app.use(express.json()); //this middleware  will parse json bodies
-
+app.use(express.json()); // parse JSON bodies
 app.use(rateLimiter);
 connectDB();
-app.use("/api/notes",notesRoute);
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-})
+// routes
+app.use("/api/notes", notesRoute);
+
+
+
+// serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../FRONTEND/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FRONTEND", "dist", "index.html"));
+  });
 }
 
-app.listen(PORT,()=>{
-    console.log("server started on port: ",PORT);
-})
-
-
-//mongodb+srv://amankumar900651_db_user:GXMIaaOwqU2GTQTv@cluster0.ztrqbft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+app.listen(PORT, () => {
+  console.log("server started on port:", PORT);
+});
